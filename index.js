4e500297e31a9e50fd6faa30b3a060ad4eaf11cf
Nicholas027 <nicholas0810152015@gmail.com>
2022-09-20 01:56:36 -0300

    'CORS'

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ import "./database/connectdb.js";
 import express from "express";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
-
+const whiteList = [process.env.ORIGIN1];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whiteList.includes(origin)) {
+        return callback(null, origin);
+      }
+      return callback("Error de CORS origin: " + origin + " No autorizado");
+    },
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
